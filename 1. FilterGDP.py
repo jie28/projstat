e025b700a14fd99486766798de615f84da9c5aa3
jie28 <wangyingjie1295@gmail.com>
2026-02-09 19:45:57 +0100

    Update 1. FilterGDP.py

--- 1. FilterGDP.py
+++ 1. FilterGDP.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv("./gdp.csv", index_col=[0])
+df = pd.read_csv("./GdpOrig.csv", index_col=[0])
 
 ef = df.drop(df.columns[[x for x in range (3, 42)]], axis=1)
 ef.drop(ef.filter(regex="Unnamed"), axis=1, inplace=True)
-ef.to_csv('./gdpnew.csv')
+ef.to_csv('./GdpFiltered.csv')
